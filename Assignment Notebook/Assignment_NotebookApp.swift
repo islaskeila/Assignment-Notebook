7c0db55e7945259f33169651749ca6d95175e1f9
Keila Islas <kislas@bsd220.org>
2024-01-30 09:54:17 -0600

    Initial Commit

---
+++ Assignment Notebook/Assignment_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_NotebookApp.swift
+//  Assignment Notebook
+//
+//  Created by Keila Islas on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
